703363c45c7b5fc67d2feafc791472f52a9fb374
Nail Gati <theproducer228@gmail.com>
2025-10-03 16:09:45 +0500

    Fixing cookie

--- frontend/src/redux/api/apiSlice.ts
+++ frontend/src/redux/api/apiSlice.ts
@@ -5,10 +5,16 @@ const BASE_URL_FALLBACK = import.meta.env.VITE_BASE_URL || "";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL_FALLBACK,
+  prepareHeaders: (headers, { getState }) => {
+    return headers;
+  },
+
+  credentials: "include",
 });
 
 export const apiSlice = createApi({
   baseQuery,
+
   tagTypes: ["Product", "Order", "User", "Category"],
   endpoints: () => ({}),
 });
