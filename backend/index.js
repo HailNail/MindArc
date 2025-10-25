7faa016f734281be2310dafb80cf1b500ea77512
Nail Gati <theproducer228@gmail.com>
2025-10-25 11:16:42 +0500

    Trying fix static files #4

--- backend/index.js
+++ backend/index.js
@@ -72,9 +72,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(staticPath));
 
   // Catch-all route (React Router support)
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(staticPath, "index.html"))
-  );
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.resolve(staticPath, "index.html"));
+  });
 }
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
